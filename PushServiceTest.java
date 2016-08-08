1d03d7ce117ffd4725b63edb76509ed07c9bc850
BanuRajadurai <rajaduraibanu@gmail.com>
2016-08-08 06:41:26 +0530

    Add files via upload

---
+++ PushServiceTest.java
@@ -0,0 +1,72 @@
+package nl.martijndwars.webpush;
+
+import com.google.common.io.BaseEncoding;
+import org.apache.http.client.fluent.Content;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.IOException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class PushServiceTest {
+    @BeforeClass
+    public static void addSecurityProvider() {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    @Test
+    public void testPushChrome() throws NoSuchProviderException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, IOException, ExecutionException, InterruptedException,JSONException {
+        Security.addProvider(new BouncyCastleProvider());
+
+        String gcmApiKey = "AIzaSyDSa2bw0b0UGOmkZRw-dqHGQRI_JqpiHug";
+        String endpoint = "https://android.googleapis.com/gcm/send/fXYSGjgKDKA:APA91bE-sqRL3us1BfdzaF0E3DKh8fwVgEFEyT7BaUH7d6j-UauS_4cSRvhrdsbYFM4-6CWLfndYLU-FBqrlX_8xa793fSVUZxzHFz7Yec_cbtM-8J0NqKF0dUjh0mVBYWAwkWZYkmRU";
+        String encodedUserPublicKey = "BB5bKjcRawntzacxKXRVMhfS60h_48ZVHWZDTEbrVufrtwsol4dMNxKvGw8HSpd770MkWi76ovbBj_mJBiLQ1SA=";
+        String encodedUserAuth = "px9ZH3w7m8tk8zuJxmeEng==";
+
+        PublicKey userPublicKey = Utils.loadPublicKey(encodedUserPublicKey);
+        byte[] userAuth = BaseEncoding.base64Url().decode(encodedUserAuth);
+
+        Notification notification = new GcmNotification(
+            endpoint,
+            userPublicKey,
+            userAuth,
+            "{\"title\": \"Hello\", \"message\": \"World\"}".getBytes()
+        );
+
+        PushService pushService = new PushService(gcmApiKey);
+        Future<Content> httpResponse = pushService.send(notification);
+
+        System.out.println(httpResponse.get().asString());
+    }
+
+    @Test
+    public void testPushFirefox() throws NoSuchProviderException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, IOException, ExecutionException, InterruptedException, JSONException {
+        Security.addProvider(new BouncyCastleProvider());
+
+        String endpoint = "https://updates.push.services.mozilla.com/push/v1/gAAAAABXS0Nhothwqf0Je2mmjuRgyXjgVylY0yZ4qmP3cglrFoneY-XOLdJuGZOsv5Eh7ndhe8mMvge3VcLhpgbQ3w6_vWK7FZkSXhzjlaIxikL6cbW6Gok5BVw1tL1jqruy5Y-deSoz";
+        String encodedUserPublicKey = "BNP6uzB5yqQDltCnO1snr-Qx3wLUPgeznuUQjfFbmehRHJK3s4eaqy04nOnm9796mceidVJPlFaobd94yjwtmpU=";
+
+        PublicKey userPublicKey = Utils.loadPublicKey(encodedUserPublicKey);
+
+        Notification notification = new Notification(
+            endpoint,
+            userPublicKey,
+            null,
+            "{\"title\": \"Hello\", \"message\": \"World\"}".getBytes()
+        );
+
+        PushService pushService = new PushService();
+        Future<Content> httpResponse = pushService.send(notification);
+
+        System.out.println(httpResponse.get().asString());
+    }
+}
---
+++ sendPush.java
@@ -0,0 +1,46 @@
+package nl.martijndwars.webpush;
+
+import com.google.common.io.BaseEncoding;
+import org.apache.http.client.fluent.Content;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.IOException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class sendPush {
+
+    public static void main(String[] args)  throws NoSuchProviderException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, IOException, ExecutionException, InterruptedException,JSONException{
+//      Security.addProvider(new BouncyCastleProvider());
+        String gcmApiKey = "AIzaSyDSa2bw0b0UGOmkZRw-dqHGQRI_JqpiHug";
+        String endpoint = "https://android.googleapis.com/gcm/send/fXYSGjgKDKA:APA91bE-sqRL3us1BfdzaF0E3DKh8fwVgEFEyT7BaUH7d6j-UauS_4cSRvhrdsbYFM4-6CWLfndYLU-FBqrlX_8xa793fSVUZxzHFz7Yec_cbtM-8J0NqKF0dUjh0mVBYWAwkWZYkmRU";
+        String encodedUserPublicKey = "BB5bKjcRawntzacxKXRVMhfS60h_48ZVHWZDTEbrVufrtwsol4dMNxKvGw8HSpd770MkWi76ovbBj_mJBiLQ1SA=";
+        String encodedUserAuth = "px9ZH3w7m8tk8zuJxmeEng==";
+
+        PublicKey userPublicKey = Utils.loadPublicKey(encodedUserPublicKey);
+        byte[] userAuth = BaseEncoding.base64Url().decode(encodedUserAuth);
+
+        Notification notification = new GcmNotification(
+            endpoint,
+            userPublicKey,
+            userAuth,
+            "{\"title\": \"Hello\", \"message\": \"World\"}".getBytes()
+        );
+
+        PushService pushService = new PushService(gcmApiKey);
+        Future<Content> httpResponse = pushService.send(notification);
+
+        System.out.println(httpResponse.get().asString());
+        System.out.println("Yes Cmg !!!");
+    }
+
+}
